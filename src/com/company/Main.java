0a51cf2566bd891642b7064ab149ce30bff79762
Polina <polinaucc@gmail.com>
2019-12-02 15:23:36 +0200

    main

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import Controller.Controller;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Controller controller = new Controller();
+        controller.process();
+    }
+}
